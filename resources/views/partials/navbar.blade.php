0175573f2eb075e9f113e966089c1afadd2d50d2
Raymond Idema <raymond@design-code.nl>
2014-09-20 00:15:24 +0200

    update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -15,6 +15,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="">{{ link_to_route('admin.domains.index', 'Domains') }}</li>
+                <li><a href="#">Settings</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li>{{ link_to_route('sessions.destroy', 'Logout') }}</li>
