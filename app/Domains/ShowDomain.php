8832dc2cfc3dd4fee671fbd1382cf8644adffaf7
Raymond Idema <raymond@design-code.nl>
2014-09-22 10:54:36 +0200

    added SSH key config

--- app/Domains/ShowDomain.php
+++ app/Domains/ShowDomain.php
@@ -1,6 +1,8 @@
 <?php  namespace Codeboard\Domains; 
 
 use Codeboard\Domains\Repositories\DomainRepository;
+use Illuminate\Contracts\Config\Config;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Log\Writer;
 
 class ShowDomain {
@@ -13,15 +15,28 @@ class ShowDomain {
      * @var \Illuminate\Log\Writer
      */
     private $log;
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+    /**
+     * @var Config
+     */
+    private $config;
 
     /**
      * @param DomainRepository $repository
      * @param Writer $log
+     * @param \Illuminate\Filesystem\Filesystem $filesystem
+     * @param \Illuminate\Contracts\Config\Config $config
      */
-    function __construct(DomainRepository $repository, Writer $log)
+    function __construct(DomainRepository $repository, Writer $log, Filesystem $filesystem, Config
+    $config)
     {
         $this->repository = $repository;
         $this->log = $log;
+        $this->filesystem = $filesystem;
+        $this->config = $config;
     }
 
     /**
@@ -33,8 +48,15 @@ class ShowDomain {
     {
 
         $domain = $this->repository->getDomainById($domainId);
+        $sshKey = $this->getPublicKey();
         $this->log->info('Show Domain id ' . $domainId, $domain->toArray());
-        return $listener->view('domains.show', compact('domain'));
+        return $listener->view('domains.show', compact('domain', 'sshKey'));
+    }
+
+    private function getPublicKey()
+    {
+        if($this->filesystem->exists($this->config->get('settings.ssh_key_location')))
+            return $this->filesystem->get($this->config->get('settings.ssh_key_location'));
     }
 
 } 
---
+++ config/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'ssh_key_location' => getenv('SSH_LOCATION'),
+];
